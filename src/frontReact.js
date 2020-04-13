63c2d6d6f39cd0471cadb363a0454043c9dcd9fb
Mxolisi Dladla <mxohdapage@gmail.com>
2020-04-14 01:31:53 +0200

    frontEnd

---
+++ src/frontReact.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const e = React.createElement;
+
+class FrontEnd extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+const domContainer = document.querySelector('#root');
+ReactDOM.render(e(FrontEnd), domContainer);
